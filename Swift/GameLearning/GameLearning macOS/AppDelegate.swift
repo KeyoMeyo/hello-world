1b03c9ecfca926e0cb1578e7ea91317f50a0394d
Tommy Stephenson <tommy@metaknight.ca>
2018-09-10 17:33:24 -0600

    initiated cross-platform game

---
+++ Swift/GameLearning/GameLearning macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  GameLearning macOS
+//
+//  Created by Tommy Stephenson on 2018-09-10.
+//  Copyright © 2018 CREER. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
