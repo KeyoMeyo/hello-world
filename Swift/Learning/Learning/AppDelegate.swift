cdcf590d442ba94f44b6b9438542232415606324
Tommy Stephenson <tommy@metaknight.ca>
2018-09-10 11:30:21 -0600

    created new learning app

---
+++ Swift/Learning/Learning/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Learning
+//
+//  Created by Tommy Stephenson on 2018-09-10.
+//  Copyright © 2018 CREER. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swift/Learning/Learning/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Learning
+//
+//  Created by Tommy Stephenson on 2018-09-10.
+//  Copyright © 2018 CREER. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
