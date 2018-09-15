a9311032ed6a9fae4cfb62a42e44e5bc6b194e21
Tommy Stephenson <tommy@metaknight.ca>
2018-09-14 23:03:25 -0600

    initiate Metal learning suite

---
+++ Swift/HelloMetal/HelloMetal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloMetal
+//
+//  Created by Tommy Stephenson on 2018-09-14.
+//  Copyright © 2018 CREER. All rights reserved.
+//
+
+import UIKit
+
+import Metal
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
